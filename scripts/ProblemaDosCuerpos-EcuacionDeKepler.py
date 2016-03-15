bc4db26f40ae28061a49e316ca03e62391b349f7
seap-udea <jorge.zuluaga@udea.edu.co>
2016-03-15 16:43:56 -0500

    New File

--- scripts/ProblemaDosCuerpos-EcuacionDeKepler.py
+++ scripts/ProblemaDosCuerpos-EcuacionDeKepler.py
@@ -3,7 +3,7 @@
 
 # # Ecuación de Kepler
 
-# In[113]:
+# In[250]:
 
 from numpy import *
 import matplotlib.pyplot as plt
@@ -11,11 +11,13 @@ from scipy.optimize import bisect
 from scipy.special import jn
 from time import time as timeit
 get_ipython().magic(u'matplotlib inline')
+DEG=pi/180
+RAD=180/pi
 
 
 # # Definición de la Ecuación
 
-# In[52]:
+# In[251]:
 
 def Ekepler(E,M,e):
     f=E-e*sin(E)-M
@@ -24,7 +26,7 @@ def Ekepler(E,M,e):
 
 # # Solución numérica
 
-# In[183]:
+# In[252]:
 
 e=0.2
 
@@ -36,30 +38,36 @@ print "E = %.10f"%(E*180/pi)
 print "Solution:\n",properties.__dict__
 
 
-# In[185]:
+# In[253]:
 
 # Rutina de solución numérica
-def Esol_numeric(M,e,xtol=1E-7):
+def Esol_numeric(M,e,xtol=1E-17):
     E=bisect(Ekepler,0,2*pi,xtol=xtol,args=(M,e))
     return E
 
 
 # # Solución Analítica
 
-# In[53]:
+# In[254]:
 
 def Esol_analytic(M,e):
     """
     Mikkola, 1991
     Code at: http://smallsats.org/2013/04/20/keplers-equation-iterative-and-non-iterative-solver-comparison/
     """
-    if e==0:return M
+    Ecorr=0;Esgn=1.0
+    if M>180*DEG:
+        M=360.0*DEG-M
+        Ecorr=360*DEG
+        Esgn=-1.0
+    if e==0:return Ecorr+Esgn*M
+        
     a=(1-e)*3/(4*e+0.5);
     b=-M/(4*e+0.5);
-    y=(b*b/4 +a*a*a/27)**0.5;
+    y=(b**2/4 +a**3/27)**0.5;
     x=(-0.5*b+y)**(1./3)-(0.5*b+y)**(1./3);
     w=x-0.078*x**5/(1 + e);
-    E=M+e*(3*w-4*x**3);
+    E=M+e*(3*w-4*w**3);
 
     #NEWTON CORRECTION 1
     sE=sin(E)
@@ -79,12 +87,14 @@ def Esol_analytic(M,e):
     f3d=-e*cE;
     f4d=e*sE;
     E=E-f/fd*(1+                  f*f2d/(2*fd*fd)+                  f*f*(3*f2d*f2d-fd*f3d)/(6*fd**4)+                  (10*fd*f2d*f3d-15*f2d**3-fd**2*f4d)*                  f**3/(24*fd**6))
+    
+    E=Ecorr+Esgn*E
     return E
 
 
 # # Solución por series
 
-# In[188]:
+# In[255]:
 
 def Esol_series_fourier(M,e):
     E=M+(e-1./8*e**3)*sin(M)+0.5*e**2*sin(2*M)+3./8*e**3*sin(3*M)
@@ -99,9 +109,9 @@ def Esol_series_bessel(M,e,N=2):
 
 # # Comparación de Soluciones
 
-# In[190]:
+# In[256]:
 
-e=0.2
+e=0.6
 Ms=linspace(0,2*pi,100)
 
 t1=timeit()
@@ -125,7 +135,7 @@ t2=timeit();print "Bessel:",(t2-t1)*1E6;t1=t2
 
 # # Gráficos comparativos
 
-# In[194]:
+# In[257]:
 
 plt.plot(Ms,Ens,'b-',label='Numeric')
 plt.plot(Ms,Eas,'r+',label='Analytic')
@@ -136,11 +146,12 @@ plt.plot(Ms,Ebs,'c+',label='Bessel Series')
 plt.legend(loc='best')
 
 
-# In[195]:
+# In[258]:
 
 plt.plot(Ms,abs(Eas-Ens),'r+',label='Analytic')
 plt.plot(Ms,abs(Efs-Ens),'g+',label='Fourier Series')
 plt.plot(Ms,abs(Ebs-Ens),'c+',label='Bessel Series')
+plt.yscale("log")
 plt.grid()
 
 
